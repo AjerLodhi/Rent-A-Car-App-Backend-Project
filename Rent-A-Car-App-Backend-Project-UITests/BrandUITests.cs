0c99013126c73bfbb6d9bdd7d66a97f6c07793ee
Ajer Lodhi <lodhia@seattleu.edu>
2023-11-26 01:33:44 +0500

    Added BrandUITests for API calls for Brands in the UI

---
+++ Rent-A-Car-App-Backend-Project-UITests/BrandUITests.cs
@@ -0,0 +1,98 @@
+// BrandUITests.cs
+// CPSC 5210 - 01
+// Purpose: Perform UI tests for Brand API for GetByid, GetAll, and Add
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Rent_A_Car_App_Backend_Project_UITests
+{
+    public class BrandUITests
+    {
+
+        IWebDriver driver;
+        string filePath;
+        [OneTimeSetUp]
+
+        public void Setup()
+
+        {
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            // Initalize file to upload & URL of website being tested
+            string localURL = "https://localhost:44320/swagger/index.html";
+
+            // Creates the ChomeDriver object, Executes tests on Google Chrome
+            driver = new ChromeDriver(path + @"\drivers\");
+            driver.Navigate().GoToUrl(localURL);
+            System.Threading.Thread.Sleep(30000);
+        }
+
+        [Test]
+        public void Title_Containing_Swagger_UI_Shows_That_App_Is_Open()
+
+        {
+            Assert.IsTrue(driver.Title.Contains("Swagger UI"));
+
+        }
+
+        [Test]
+        public void Click_GetById_For_Brands_Should_Return_TryItOut()
+        {
+            // Arrange
+            // Wait for the Swagger UI to load (adjust the wait time as needed)
+            System.Threading.Thread.Sleep(5000);
+            // Find the "Brands" tag and click to expand
+            IWebElement brandsTag = driver.FindElement(By.Id("operations-Brands-get_api_Brands_getbyid"));
+            
+            // Act
+            brandsTag.Click();
+            // Wait for the operation details to load (adjust the wait time as needed)
+            System.Threading.Thread.Sleep(2000);
+            // Find the "Try it Out" button
+            IWebElement tryItOutButton = driver.FindElement(By.XPath("//button[contains(text(),'Try it out')]"));
+
+            // Assert
+            // Assert that the "Try it Out" button is displayed
+            Assert.IsTrue(tryItOutButton.Displayed, "Try it Out button is not displayed");
+        }
+
+        [Test]
+        public void Click_TryOut_And_Execute_For_Brands_Should_Return_Loading()
+        {
+            // Arrange
+            IWebElement tryItOutButton = driver.FindElement(By.XPath("//button[contains(text(),'Try it out')]"));
+            tryItOutButton.Click();
+            // Wait for the operation details to load (adjust the wait time as needed)
+            System.Threading.Thread.Sleep(2000);
+            // Locate the second input using XPath indexing (index starts from 1)
+            var secondInput = driver.FindElement(By.XPath("(//input[@type='text'])[1]"));
+            // Perform actions on the second input
+            secondInput.SendKeys("4");
+
+            //Act
+            // Locate the button by its class name
+            var executeButton = driver.FindElement(By.XPath($"//button[contains(text(), 'Execute')]"));
+            // Click on the "Execute" button
+            executeButton.Click();
+
+            // Assert
+            // Assert that loading is displayed
+            System.Threading.Thread.Sleep(2000);
+            var loadingElement = driver.FindElement(By.ClassName("loading"));
+
+            // Assert that the loading element is displayed
+            Assert.IsTrue(loadingElement.Enabled);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+
+        {
+            driver.Quit();
+
+        }
+
+    }
+}
