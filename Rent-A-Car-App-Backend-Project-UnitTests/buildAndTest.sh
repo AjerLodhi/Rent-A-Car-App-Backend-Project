70089c46fd5fc0a15ef41494201b7026d3f0f79d
Ajer Lodhi <lodhia@seattleu.edu>
2023-11-28 08:25:42 +0500

    Added buildAndTest.sh for Regression testing

---
+++ Rent-A-Car-App-Backend-Project-UnitTests/buildAndTest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Build the solution
+dotnet build
+
+# Run unit tests
+dotnet test --no-build
+
+
+send_email() {
+    Success=$1
+    ToEmail="lodhia@seattleu.edu"  # Replace with the recipient's email
+
+    EmailBody=$(cat <<EOF
+Build Succeeded: $Success
+Tests Passed: $Success
+EOF
+)
+
+    # Send email using mail command (you may need to configure mail on your system)
+    echo "$EmailBody" | mail -s "Test Suite Status" "$ToEmail"
+}
+
+# Check the exit code of the last command
+if [ $? -eq 0 ]; then
+    # Tests passed, send success email
+    echo "Tests Passed: True"
+    send_email true
+else
+    # Tests failed, send failure email
+    echo "Tests Passed: False"
+    send_email false
+fi
+
+
